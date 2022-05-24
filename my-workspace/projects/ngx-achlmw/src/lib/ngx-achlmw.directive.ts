af808d3811b565bca25d23a48741c823ff07a47b
narendrasinghrathore <rathoreofficialemail@gmail.com>
2022-05-24 17:13:09 +0530

    Highlight text based on matching

--- my-workspace/projects/ngx-achlmw/src/lib/ngx-achlmw.directive.ts
+++ my-workspace/projects/ngx-achlmw/src/lib/ngx-achlmw.directive.ts
@@ -1,85 +1,52 @@
 import {
   Directive,
   Renderer2,
-  TemplateRef,
-  ViewContainerRef,
-  AfterContentInit,
   ElementRef,
   Input,
   OnChanges,
   SimpleChanges,
-  OnInit,
-  OnDestroy,
 } from '@angular/core';
 
 @Directive({
   selector: '[libNgxAchlmw]',
 })
-export class NgxAchlmwDirective
-  implements OnInit, AfterContentInit, OnChanges, OnDestroy
-{
+export class NgxAchlmwDirective implements OnChanges {
+  readonly searchPropName = 'searchTerm';
+  readonly optionTextPropName = 'optionText';
   @Input()
   searchTerm: string = '';
+
+  @Input()
+  optionText: string = '';
 
   originalHTML: any;
   spanMatOption: any;
   parentNode: any;
 
-  constructor(
-    private el: ElementRef,
-    private viewContainer: ViewContainerRef,
-    private render: Renderer2
-  ) {}
-
-  ngOnDestroy(): void {
-    // console.log(this.el.nativeElement,this.originalText);
-  }
-  ngOnInit(): void {
-    // console.log(this.el.nativeElement.querySelector(
-    //   'span.mat-option-text'
-    // ))
-    // this.originalText = this.el.nativeElement.querySelector(
-    //   'span.mat-option-text'
-    // ).textContent;
-    // this.render.
-  }
+  constructor(private el: ElementRef, private render: Renderer2) {}
 
   ngOnChanges(changes: SimpleChanges): void {
-    const value: string = changes['searchTerm'].currentValue;
-    if ((!value || value.length === 0) && this.originalHTML) {
-      this.replaceAndAddSpanMatOption(this.originalHTML);
-    } else {
-      const regex = new RegExp(value, 'gi');
-      const elementToRemove = this.el.nativeElement.querySelector(
-        'span.mat-option-text'
-      );
-      const term: string = elementToRemove.textContent;
-      if (!term) return;
-      const newValue = term.replace(
-        regex,
-        (match) => `<span style="color:purple">${match}</span>`
-      );
+    const searchTermObj = changes[this.searchPropName];
+    if (
+      searchTermObj.currentValue === null ||
+      searchTermObj.currentValue === undefined
+    )
+      return;
 
-      this.replaceAndAddSpanMatOption(newValue);
-    }
-  }
+    // Start updating
 
-  replaceAndAddSpanMatOption(newValue: string) {
-    console.log(newValue, this.originalHTML, this.parentNode);
-    this.render.removeChild(this.parentNode, this.spanMatOption);
-    const span = this.render.createElement('span');
-    this.render.addClass(span, 'mat-option-text');
-    this.render.setProperty(span, 'innerHTML', newValue);
-
-    this.render.appendChild(this.parentNode, span);
-  }
-
-  ngAfterContentInit(): void {
-    console.count('ngAfterContentInit');
-    this.parentNode = this.el.nativeElement;
-    this.spanMatOption = this.el.nativeElement.querySelector(
-      'span.mat-option-text'
+    let searchTerm = searchTermObj.currentValue;
+    const regex = new RegExp(searchTerm, 'gi');
+    const span = this.el.nativeElement.querySelector('span.mat-option-text');
+    const spanTextContent = span.textContent;
+    let newNodeValue = spanTextContent.replace(
+      regex,
+      (match: string) => `<span style="color:purple">${match}</span>`
     );
-    this.originalHTML = this.spanMatOption.innerHTML;
+    let value =
+      regex.test(spanTextContent) && searchTermObj.currentValue !== ''
+        ? newNodeValue
+        : this.optionText;
+    this.render.setProperty(span, 'innerHTML', value);
   }
 }
